d849bb117b662bb537f3b5eda150f62f1849ac3c
Buhay Malaya <buhaymalaya@Buhays-Air.lan>
2024-04-26 09:18:29 -0400

    cors resources did not resolve issue

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 api = Api(app)
 jwt = JWTManager(app)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173/"}})
+CORS(app)
+    #  resources={r"/*": {"origins": "http://localhost:5173/"}})
 db = SQLAlchemy(app) #instantiate
 migrate = Migrate(app, db) 
 
