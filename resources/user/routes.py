e08efc02a18440262593544391c3e5723723bfe2
Buhay Malaya <buhaymalaya@Buhays-Air.lan>
2024-04-30 09:38:33 -0400

    changed url endpoint from id to username for user routes

--- resources/user/routes.py
+++ resources/user/routes.py
@@ -29,58 +29,65 @@ class UserList(MethodView):
             abort(400, message="Username or email has already been taken. Please try a different one.")
 
 
-
-# tested, nothing in json body, remember to input user_id
+# changed userid to username bc react indicates undefined
+# tested, nothing in json body
 # in url field
-@bp.route('/user/<int:id>')
+@bp.route('/user/<username>')
 class User(MethodView):
 
     @bp.response(200, UserSchema)
-    def get(self, id):
-        user = UserModel.query.get(id)
+    def get(self, username):
+        user = UserModel.query.filter_by(username=username).first()
         if user:
             return user
         else:
             abort(400, msg='Invalid entry; please try again')
 
-    # tested, once logged in, input fields same as post
+    # tested, once logged in, input fields same as userschema
     @jwt_required()
     @bp.arguments(UserSchema)
     @bp.response(200, UserSchema)
-    def put(self, data, id):
-        user = UserModel.query.get(id)
+    def put(self, data, username):
+        user = UserModel.query.filter_by(username=username).first()
         if user:
-            user.from_dict(data)
+            data = request.json
+            user.username = data.get('username', user.username)
+            user.email = data.get('email', user.email)
+            user.first_name = data.get('first_name', user.first_name)
+            user.last_name = data.get('last_name', user.last_name)
+            user.zip_code = data.get('zip_code', user.zip_code)
             user.save_user()
-            return user
+            return user, 204
+            
         else:
-            abort(400, message='Invalid entry; please try again')
+            abort(404, message='User not found')
      
     # tested, log in, input user_id in url field
+    # delete won't work bc of replies constraint..TBD
     @jwt_required()
-    def delete(self, id):
-        user = UserModel.query.get(id)
+    def delete(self, username):
+        user = UserModel.query.filter_by(username=username).first()
         if user:
             user.del_user()
-            return {'Message': 'User deleted'}, 200
-        abort(400, message='Invalid entry; please try again')
+            return {'Message': username + 'deleted'}, 200
+        abort(404, message='Error: User not found')
         
 
 
 # login and logout
 
 # tested
-@bp.route('/login', methods=['POST', 'PUT', 'DELETE'])
+@bp.route('/login', methods=['POST', 'PUT', 'DELETE', 'GET'])
 def login():
     login_data = request.get_json()
 
     username = login_data['username']
     user = UserModel.query.filter_by(username = username).first()
     if user and user.check_password(login_data['password']):
-        access_token = create_access_token(identity=user.id)
-        return {'access_token': access_token}, 201
+        access_token = create_access_token(identity=user.username)
+        return {'access_token': access_token, 'username': username}, 201
     
-    abort(Message="Invalid user; please try again")
+    abort(401, Message="Invalid username or password; please try again")
 
 # tested
 @bp.route("/logout", methods=["POST"])
